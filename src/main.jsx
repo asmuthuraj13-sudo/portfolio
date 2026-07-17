e978d09c320c15e1c7611dd733bfbdeb61a4fec6
asmuthuraj13-sudo <asmuthuraj13@gmail.com>
2026-07-17 10:26:15 +0530

    Fix: correct case for Styles import (Vercel build)

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 
-import "./styles/variables.css";
+import "./Styles/variables.css";
 import "./index.css";
 
 import React from "react";
